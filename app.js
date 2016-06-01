cf4c2eb6bfe9dd4edeacffafe72fba9c5601e989
morrxy <morrxy@gmail.com>
2016-06-01 11:41:51 +0800

    middleware

--- app.js
+++ app.js
@@ -3,9 +3,12 @@
 const express = require('express');
 const app = express();
 
+app.all('*', require('./middleware/route-test'), require('./middleware/session-test'));
+
 app.use('/', require('./route/index'));
 app.use('/user', require('./route/user'));
 app.use('/mission', require('./route/mission'));
+app.use('/secret', require('./route/secret'));
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
