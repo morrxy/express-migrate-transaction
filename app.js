c87e0dd00dcf602a9b3c1a9cfded9d41c137bd9c
morrxy <morrxy@gmail.com>
2016-06-03 17:35:01 +0800

    restfull todos

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.use('/user', require('./route/user'));
 app.use('/mission', require('./route/mission'));
 app.use('/secret', require('./route/secret'));
 app.use('/todo', require('./route/todo'));
+app.use('/todos', require('./route/todos'));
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
--- model/todo.js
+++ model/todo.js
@@ -4,6 +4,7 @@ const obj = {};
 module.exports = obj;
 
 obj.list = require('./todo/list');
+obj.single = require('./todo/single');
 obj.add = require('./todo/add');
 obj.remove = require('./todo/remove');
 obj.update = require('./todo/update');
